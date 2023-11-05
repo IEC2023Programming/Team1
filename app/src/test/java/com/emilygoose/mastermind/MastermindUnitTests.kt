6cc49e806524eaa6172cd39e96c93e9c824237f0
Emily Goose <EmilyGoose@users.noreply.github.com>
2023-11-05 12:22:52 -0500

    Added more unit tests

--- app/src/test/java/com/emilygoose/mastermind/MastermindUnitTests.kt
+++ app/src/test/java/com/emilygoose/mastermind/MastermindUnitTests.kt
@@ -25,6 +25,14 @@ class MastermindUnitTests {
     }
 
     @Test
+    fun guess_HalfHalf() {
+        // Guess that is two black, two white
+        val guess = secretCode.subList(0, 2) + secretCode.subList(2, 4).reversed()
+        val resultPair = viewModel.getPegs(guess.toColors(), secretCode.toColors())
+        assertEquals(resultPair, Pair(2, 2))
+    }
+
+    @Test
     fun guess_allWrong() {
         // Guess that is completely distinct from the secret code
         val guess = listOf(1, 1, 1, 1)
