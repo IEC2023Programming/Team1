ad0d5e0faf2d957929019cebe8a4ab69e11da21d
Emily Goose <EmilyGoose@users.noreply.github.com>
2023-11-05 11:45:28 -0500

    Design changes

--- app/src/main/java/com/emilygoose/mastermind/MainActivity.kt
+++ app/src/main/java/com/emilygoose/mastermind/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.emilygoose.mastermind.ui.GuessRow
 import com.emilygoose.mastermind.ui.theme.MastermindTheme
 
@@ -24,39 +29,49 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
-                    Column(
-                        modifier = Modifier.fillMaxWidth()
+                    Box(
+                        contentAlignment = Alignment.Center
                     ) {
-                        for (guess in viewModel.guesses) {
-                            GuessRow(
-                                guess = guess,
-                                pegs = viewModel.getPegs(guess),
-                                locked = true
-                            )
-                        }
-                        // Show unlocked guessable row if allowed to guess
-                        if (viewModel.canGuess.value) {
-                            GuessRow(
-                                guess =
-                                // Map indices from the mutableList to colors
-                                viewModel.currentGuess.map { index ->
-                                    GuessColor.values()[index]
-                                },
-                                locked = false,
-                                // Callback to increment guess colors on click
-                                onColorClick = { index ->
-                                    viewModel.incrementGuess(index)
-                                },
-                                // Callback for when submit button is clicked
-                                onSubmit = {
-                                    viewModel.submitGuess()
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            OutlinedCard(
+                                modifier = Modifier
+                                    .height(640.dp)
+                                    .width(352.dp)
+                            ) {
+                                for (guess in viewModel.guesses) {
+                                    GuessRow(
+                                        guess = guess,
+                                        pegs = viewModel.getPegs(guess),
+                                        locked = true
+                                    )
                                 }
-                            )
-                        } else {
-                            // Show the user the actual answer if they've reached max guesses or won
-                            GuessRow(guess = viewModel.secretCode, locked = true)
+                            }
+                            // Show unlocked guessable row if allowed to guess
+                            if (viewModel.canGuess.value) {
+                                GuessRow(
+                                    guess =
+                                    // Map indices from the mutableList to colors
+                                    viewModel.currentGuess.map { index ->
+                                        GuessColor.values()[index]
+                                    },
+                                    locked = false,
+                                    // Callback to increment guess colors on click
+                                    onColorClick = { index ->
+                                        viewModel.incrementGuess(index)
+                                    },
+                                    // Callback for when submit button is clicked
+                                    onSubmit = {
+                                        viewModel.submitGuess()
+                                    }
+                                )
+                            } else {
+                                // Show the user the actual answer if they've reached max guesses or won
+                                GuessRow(guess = viewModel.secretCode, locked = true)
+                            }
                         }
                     }
                 }
