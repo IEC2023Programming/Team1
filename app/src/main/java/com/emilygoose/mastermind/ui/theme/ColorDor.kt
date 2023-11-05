dc6c755e88d112af2a820b8f443542324d3cfeda
Emily Goose <EmilyGoose@users.noreply.github.com>
2023-11-05 09:46:42 -0500

    Made ColorDot more reusable

---
+++ app/src/main/java/com/emilygoose/mastermind/ui/theme/ColorDor.kt
@@ -0,0 +1,25 @@
+package com.emilygoose.mastermind.ui.theme
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Circle
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+
+@Composable
+fun ColorDot(color: Color, modifier: Modifier) {
+    Box(
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Circle,
+            contentDescription = "Guess dot",
+            tint = color,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
